1d27fb79df1527bfad37c5a69b08b9260e041858
Miguelito2774 <149016550+Miguelito2774@users.noreply.github.com>
2024-02-26 10:38:05 -0400

    terminando

--- 26febrero/cadenas.hs
+++ 26febrero/cadenas.hs
@@ -1,8 +1,9 @@
 parenthesizedFunction :: String -> [String]
 parenthesizedFunction "" = [""]
+parenthesizedFunction s = words s
 
 split :: String -> Char -> [String]
-split [] _ = []
+split [] _ = [""]
 split (x : xs) c
   | x == c = split xs c
   | otherwise = (x : takeWhile (/= c) xs) : split (dropWhile (/= c) xs) c
